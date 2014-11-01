1c9b416c1a4fc4fd05fa03492172ff0bd0456aaf
Taimur <pain@live.co.za>
2014-11-01 18:26:38 +0000

    asfasf

--- mean-master/public/modules/core/controllers/home.client.controller.js
+++ mean-master/public/modules/core/controllers/home.client.controller.js
@@ -8,3 +8,59 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	}
 
 ]);
+
+angular.module('core').controller('HomeController', function ($scope) {
+  $scope.tabs = [{
+	name: 'Default',
+	created: '637263872163',
+	user: {
+		displayName: 'Taimur',
+		phonenumber: 'hjdhsjkdahjks'
+	},
+	submenus: {
+		'1': {
+			text: 'Press one to call 911',
+			action: 'submenu',
+			submenu: {
+				'-1': {
+					action: 'redirect',
+					phonenumber: '911'
+				}
+			}
+		},
+		'2': {
+			text: 'Press two to leave a voice message',
+			action: 'voicemail'
+		}
+	}
+},{
+	name: 'Default2',
+	created: '637263872163',
+	user: {
+		displayName: 'Taimur',
+		phonenumber: 'hjdhsjkdahjks'
+	},
+	submenus: {
+		'1': {
+			text: 'Press one to call 911',
+			action: 'submenu',
+			submenu: {
+				'-1': {
+					action: 'redirect',
+					phonenumber: '911'
+				}
+			}
+		},
+		'2': {
+			text: 'Press two to leave a voice message',
+			action: 'voicemail'
+		}
+	}
+}];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      alert('You\'ve selected the alert tab!');
+    });
+  };
+});
