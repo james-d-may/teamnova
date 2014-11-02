ff50fd27796db32e3c0a07fbba182f751d5bd6b4
James May <james.may@keble.ox.ac.uk>
2014-11-02 08:46:11 +0000

    jife

--- variables.php
+++ variables.php
@@ -10,24 +10,14 @@
     $companyemail = $obj["username"];
 
     $activeActivity = $obj["activeMenu"]["options"];
-    var_dump($obj["activeMenu"]["options"]);
-    echo "@@@@@@@@@@@@@@";
     $busyActivity = $obj["busyMenu"]["options"];
-    var_dump($obj["busyMenu"]["options"]);
-    echo "@@@@@@@@@@@@@@";
     $isActive = $obj["isActive"];
-    var_dump($obj["isActive"]);
 
     $nochoices = if ($isActive)
     {
       sizeof($activeActivity);
     } else {
       sizeof($busyActivity);
-    }
-
-    if ($isActive) {
-        $choice1text =
     } 
- 
 
 ?>
